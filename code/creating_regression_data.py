af660e6f8a1208b1ba9106cba191614646f6f5bd
Michael ODonnell <36376324+odonnell31@users.noreply.github.com>
2020-11-30 14:33:05 -0500

    furnished regression dataset
    added csv

--- code/creating_regression_data.py
+++ code/creating_regression_data.py
@@ -15,111 +15,14 @@ nba_data = pd.read_csv("https://raw.githubusercontent.com/odonnell31/NBA-Team-St
 
 # create dataframe for only nba finals teams
 nba_finals_teams = nba_data[nba_data['Finals_Team'] == 'Y']
-nba_finals_teams = nba_finals_teams[nba_finals_teams['Year'] > 2012]
+nba_finals_teams = nba_finals_teams[nba_finals_teams['Year'] > 1998]
 nba_finals_teams = nba_finals_teams.reset_index(drop=True)
 
-"""
-# calc years since last losing season
-years_since_losing = []
-consecutive_years_losing = []
-consecutive_playoffs = []
-for i in range(len(nba_finals_teams)):
-    finals_year = nba_finals_teams['Year'][i]
-    year_prior = finals_year - 1
-    team = nba_finals_teams['Team'][i]
-    last_losing_season = 0
-    consec_losing_seasons = 0
-    consec_playoffs = 0
-    # flags
-    losing = 0
-    playoffs = 0
-    
-    # if the last season was a losing season:
-    if nba_data.loc[(nba_data['Team'] == team) & (nba_data['Year'] == year_prior)]['Losing_season'].values[0] == "Y":
-        last_losing_season = year_prior
-        consec_losing_seasons += 1
-        losing = 1
-        year_prior -= 1
-        
-        # how many consecutive losing seasons were there?
-        while losing == 1:
-            if nba_data.loc[((nba_data['Team'] == team) & (nba_data['Year'] == year_prior))]['Losing_season'].values[0] == 'Y':
-                consec_losing_seasons += 1
-                year_prior -= 1
-            else:
-                losing = 0
-    
-    # if the last season was a winning season            
-    elif nba_data.loc[((nba_data['Team'] == team) & (nba_data['Year'] == year_prior))]['Losing_season'].values[0] == 'N':
-        
-        # if the last season was also a playoffs seasons
-        if nba_data.loc[((nba_data['Team'] == team) & (nba_data['Year'] == year_prior))]['Playoffs'].values[0] == 'Y':
-            playoffs = 1
-            consec_playoffs += 1
-            year_prior -= 1
-            
-            # how many consecutive playoffs years were there?
-            while playoffs == 1:
-                
-                # if previous season was playoffs season, add it.
-                if nba_data.loc[((nba_data['Team'] == team) & (nba_data['Year'] == year_prior))]['Playoffs'].values[0] == 'Y':
-                    consec_playoffs += 1
-                    year_prior -= 1
-                else:
-                    playoffs = 0
-                    
-            # if its no longer a playoffs streak, was it a losing season
-            if nba_data.loc[(nba_data['Team'] == team) & (nba_data['Year'] == year_prior)]['Losing_season'].values[0] == "Y":
-                last_losing_season = year_prior
-                consec_losing_seasons += 1
-                losing = 1
-                year_prior -= 1
-                
-        # if it was not a playoffs season, was it winning or losing?
-        else:
-            playoffs = 0
-            year_prior -= 1
-                    
-            while losing == 0:
-                
-                if nba_data.loc[((nba_data['Team'] == team) & (nba_data['Year'] == year_prior))]['Losing_season'].values[0] == 'N':
-                    year_prior -= 1
-                    
-                else:
-                    last_losing_season = year_prior
-                    consec_losing_seasons += 1
-                    losing = 1
-                    year_prior -= 1
-                    
-                    while losing == 1:
-                        if nba_data.loc[((nba_data['Team'] == team) & (nba_data['Year'] == year_prior))]['Losing_season'].values[0] == 'Y':
-                            consec_losing_seasons += 1
-                            year_prior -= 1
-                        else:
-                            losing = 0
-  
-            
-                    
-    years_since_losing.append(finals_year - last_losing_season)
-    consecutive_years_losing.append(consec_losing_seasons)
-    consecutive_playoffs.append(consec_playoffs)
-                
-nba_finals_teams['years_since_losing_season'] = years_since_losing
-nba_finals_teams['consecutive_years_losing'] = consecutive_years_losing
-nba_finals_teams['consecutive_playoffs'] = consecutive_playoffs
-            
-"""
-
-# calc number of consecutive losing seasons
-
-
-# calc number of consecutive playoff seasons leading to final appearance
-
-
-# calc years since last losing season
+# calc years since last losing season, and how many consecutibe losing season
 years_since_losing = []
 consecutive_years_losing = []
 
+# iterate through all rows in dataframe
 for i in range(len(nba_finals_teams)):
     finals_year = nba_finals_teams['Year'][i]
     year_prior = finals_year - 1
@@ -149,31 +52,75 @@ for i in range(len(nba_finals_teams)):
         year_prior -= 1
         losing = 0
         
+        # work backwards to find the last losing season
         while losing == 0:
+            
+            # if it wasn't a losing season, keep looking
             if nba_data.loc[((nba_data['Team'] == team) & (nba_data['Year'] == year_prior))]['Losing_season'].values[0] == 'N':
                 year_prior -= 1
                 losing = 0
-                
+            
+            # if it was a losing season, flag it
             else:
                 last_losing_season = year_prior
                 consec_losing_seasons += 1
                 year_prior -= 1
                 losing = 1
-                
+        
+        # now, count the consecutive losing seasons      
         while losing == 1:
             if nba_data.loc[((nba_data['Team'] == team) & (nba_data['Year'] == year_prior))]['Losing_season'].values[0] == 'Y':
                 consec_losing_seasons += 1
                 year_prior -= 1
                 losing = 1
-                
+            
+            # stop counting when they win again
             else:
                 losing = 0
             
                     
     years_since_losing.append(finals_year - last_losing_season)
     consecutive_years_losing.append(consec_losing_seasons)
-    #consecutive_playoffs.append(consec_playoffs)
                 
 nba_finals_teams['years_since_losing_season'] = years_since_losing
 nba_finals_teams['consecutive_years_losing'] = consecutive_years_losing
-#nba_finals_teams['consecutive_playoffs'] = consecutive_playoffs
+
+# calc years of consecutive playoff appearances pre-finals appearance
+consecutive_playoff_seasons = []
+
+# iterate through all rows in dataframe
+for i in range(len(nba_finals_teams)):
+    finals_year = nba_finals_teams['Year'][i]
+    year_prior = finals_year - 1
+    team = nba_finals_teams['Team'][i]
+    consec_playoff_seasons = 0
+    # flags
+    playoffs = 0
+    
+    # if the last season was a playoffs season:
+    if nba_data.loc[(nba_data['Team'] == team) & (nba_data['Year'] == year_prior)]['Playoffs'].values[0] == "Y":
+        consec_playoff_seasons += 1
+        year_prior -= 1
+        playoffs = 1
+        
+        # how many consecutive playoffs seasons were there?
+        while playoffs == 1:
+            try:
+                if nba_data.loc[((nba_data['Team'] == team) & (nba_data['Year'] == year_prior))]['Playoffs'].values[0] == 'Y':
+                    consec_playoff_seasons += 1
+                    year_prior -= 1
+                else:
+                    playoffs = 0
+            except:
+                print("ran out of seasons...")
+                playoffs = 0
+    
+    else:
+        print("The", team, "did not make the playoffs prior to their",
+              finals_year, "finals appearance")
+                    
+    consecutive_playoff_seasons.append(consec_playoff_seasons)
+                
+nba_finals_teams['consecutive_playoff_seasons'] = consecutive_playoff_seasons
+
+nba_finals_teams.to_csv("nba_finals_teams_data_1990_v2.csv", index=False)
